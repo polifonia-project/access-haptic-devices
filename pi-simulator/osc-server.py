5e692db55128d3f7e7caa11a81351239921539f9
Jason Carvalho <jase@jasoncarvalho.com>
2022-01-27 14:33:36 +0000

    testing

--- pi-simulator/osc-server.py
+++ pi-simulator/osc-server.py
@@ -29,6 +29,7 @@ if __name__ == "__main__":
   dispatcher.map("/filter", print)
   dispatcher.map("/volume", print_volume_handler, "Volume")
   dispatcher.map("/logvolume", print_compute_handler, "Log volume", math.log)
+  dispatcher.set_default_handler(print)
 
   server = osc_server.ThreadingOSCUDPServer(
       (args.ip, args.port), dispatcher)
